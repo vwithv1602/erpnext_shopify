e04c8a8c5f88ee3af86b49d015a44b7179eaca23
root <root@ip-172-31-44-140.us-east-2.compute.internal>
2018-02-21 10:05:44 +0000

    Item Images and other changes in old production

---
+++ erpnext_shopify/vlog.py
@@ -0,0 +1,12 @@
+import datetime
+def vwrite(contenttowrite):
+    target = open("vshopifylogfile.txt", 'a+')
+    target.write("\n==========================="+str(datetime.datetime.now())+"===========================\n")
+    target.write("\n"+str(contenttowrite)+"\n")
+    target.close()
+
+def ebaydebug(contenttowrite):
+    target = open("debugshopify.txt", 'a+')
+    target.write("\n===========================" + str(datetime.datetime.now()) + "===========================\n")
+    target.write("\n" + str(contenttowrite) + "\n")
+    target.close()
